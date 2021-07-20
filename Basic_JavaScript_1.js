00689a43804cfd66193fbb369cadbec5ab1554b1
JacobDrawhorn <jake.drawhorn@gmail.com>
2021-07-20 14:40:59 -0700

    BASIC HTML AND JAVASCRIPT 1 SUBMISSION ASSIGNMENT

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Error.");
+function My_First_Function() {
+		var String = "Kiss me, I'm German!";
+		var result = String.fontcolor("red");
+		document.getElementById("German").innerHTML = 
+		result;
+	}
+	
